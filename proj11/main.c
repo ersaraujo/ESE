2f3bce5fb4d51da88f76d33219a3827ed91a14b6
Elisson Araújo <44985807+ersaraujo@users.noreply.github.com>
2022-05-16 17:09:16 -0300

    Update main.c
    Versão final

--- proj11/main.c
+++ proj11/main.c
@@ -2,11 +2,11 @@
 
 #define ROW_SIZE        4
 #define COL_SIZE        3
-#define CORRECAO		0
+#define CORRECAO				0
 #define BOUCING_TIME    4
 
-#define FREQ_TIMER 			200
-#define FREQ_CLK 			12000000
+#define FREQ_TIMER 			100
+#define FREQ_CLK 				12000000
 
 #define TH0_INICIAL ((65536-(FREQ_CLK/(12*FREQ_TIMER)) + CORRECAO)>>8)
 #define TL0_INICIAL	((65536-(FREQ_CLK/(12*FREQ_TIMER)) + CORRECAO)&0xFF)
@@ -35,18 +35,25 @@ bit teclaApertada = 0;
 void initializeSerial(){	// Using UART 1
   S0RELH = 0x03;
   S0RELL = 0xD9;
+	PCON |= 0x80;
   BD = 1;
 	S0CON |= 0x50;
 	TMOD  = (TMOD&0x0F) | 0x20;
-	ES0 = 1;	
 }
 void timerInit(){
 	TR0 = 0;																// Para o timer
-	TMOD = (TMOD & 0xF0)|0x01;							//Define modo
+	TMOD = (TMOD & 0xF0)|0x01;							// Define modo
 	TH0 = TH0_INICIAL;											// Define valores iniciais para os registradores TH0 e TL0
 	TL0 = TL0_INICIAL;
 	ET0 = 1; 																// Habilita interrupcao do timer 0
 	TR0 = 1;																// Ativa o timer
+}
+
+void intSerial (void) interrupt 4
+{
+	if(TI0){
+		TI0=0;
+	}
 }
 
 void isr_timer0 (void) interrupt 1 {
@@ -58,10 +65,11 @@ void isr_timer0 (void) interrupt 1 {
 	switch(estado){
 		case NTTA: 
         P5 = ~(1 << (bit_de_varredura & 0x03));
-				if(!(P5&(1<<leitura+4))){
-					ln = leitura;
-					cl = bit_de_varredura;
+				if(!(P5&(1<<leitura+4))){									// Verifica os bits de leitura de P5 (P5.5, P5.6, P5.7)
+					ln = bit_de_varredura;									
+					cl = leitura;
 					teclaApertada = 1;
+					S0BUF = keys[ln][cl];
           estado = WAITING_1;
         }else{
           bit_de_varredura++;
@@ -99,19 +107,10 @@ void isr_timer0 (void) interrupt 1 {
     }
 }
 
-void writeSerial(char i, char j){
-    S0BUF = keys[i][j];
-    while(!TI0);
-		TI0 = 0;
-		teclaApertada = 0;
-}
 
 int main(){
     timerInit();
+		initializeSerial();
 		EAL = 1;
-    while(1){
-        if(teclaApertada){
-            writeSerial(ln, cl);
-        }
-    }
+    while(1);
 }
